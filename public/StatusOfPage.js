a73c94f556ca4eecf5e5185d1e4071eac8d8e900
Vasiliy Pupkin <led2017l@gmail.com>
2020-05-15 14:44:22 +0300

    Mak ei

--- public/StatusOfPage.js
+++ public/StatusOfPage.js
@@ -3,18 +3,17 @@
         loadElem:document.querySelector('#loadingPage'),
         customEv:new CustomEvent('page-loaded',{bubbles:true,cancelable:false}),
         dataStart: Date.now(),
-        change(bool){              
-                this.loadElem.setAttribute('style','display:none !important')
-                this.loadElem.setAttribute('data-loaded',bool);
-                document.body.style.overflowY="scroll"
-                this.loadElem.firstElementChild.classList.remove('loading')
+        change(){
+            this.loadElem.setAttribute('style','display:none !important')
+            this.loadElem.setAttribute('data-loaded',arguments[0]);
+            document.body.style.overflowY="scroll"
+            this.loadElem.firstElementChild.classList.remove('loading')
         },
         loading(){
-            console.log(this.dataStart-window.timeStart)
-            if(document.readyState=="complete" ){
-                this.change(true);
+            if(document.readyState=="complete"){
+                this.change(true)
                 this.loadElem.dispatchEvent(this.customEv)
-            }            
+            }
         }
     }
     Object.freeze(obj)
