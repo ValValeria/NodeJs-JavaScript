b7f46dd9d33d9d98cc1c33ef9b9d600de1dba786
Vasiliy Pupkin <led2017l@gmail.com>
2020-05-15 14:27:19 +0300

    Make it better

--- public/StatusOfPage.js
+++ public/StatusOfPage.js
@@ -2,15 +2,22 @@
     const obj={
         loadElem:document.querySelector('#loadingPage'),
         customEv:new CustomEvent('page-loaded',{bubbles:true,cancelable:false}),
-        loading(){
-            if(document.readyState=="complete"){
+        dataStart: Date.now(),
+        change(bool){              
                 this.loadElem.setAttribute('style','display:none !important')
-                this.loadElem.setAttribute('data-loaded','true');
+                this.loadElem.setAttribute('data-loaded',bool);
                 document.body.style.overflowY="scroll"
-                this.loadElem.dispatchEvent(this.customEv)
                 this.loadElem.firstElementChild.classList.remove('loading')
+        },
+        loading(){
+            if(window.timeStart-this.dataStart>2 ){
+               document.querySelector('#somespan').textContent="Видео не удалось загрузить"
+              setTimeout(()=>this.change(false),2000);
             }
-            
+            else if(document.readyState=="complete"){
+                this.change(true);
+                this.loadElem.dispatchEvent(this.customEv)
+            }            
         }
     }
     Object.freeze(obj)
